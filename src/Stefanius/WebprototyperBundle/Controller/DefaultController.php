b733b3216c44b5d44c1a84509f0621043ab2bdf2
Stefanius <sgrootveld@gmail.com>
2014-01-18 20:22:50 +0100

    Updated routing

--- src/Stefanius/WebprototyperBundle/Controller/DefaultController.php
+++ src/Stefanius/WebprototyperBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace Stefanius\WebprototyperBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+	
+	/**
+	 * @Route("/lullo")
+	 */
+	public function indexAction()
+	{
+		return $this->render('StefaniusWebprototyperBundle:Default:index.html.twig', array('name' => 'banaan'));
+	}
+	
+	/**
+	 * @Route("/search/{name}")
+	 */
+    public function testAction($name)
     {
         return $this->render('StefaniusWebprototyperBundle:Default:index.html.twig', array('name' => $name));
     }
+
+
+
 }
